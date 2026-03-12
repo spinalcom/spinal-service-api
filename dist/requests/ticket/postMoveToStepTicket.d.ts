5e59f15e9d38f46ba79fcaf391409d923d389c08
spinalcom <a.abdouchakour@spinalcom.com>
2026-03-12 12:00:51 +0100

    Rebuild

--- dist/requests/ticket/postMoveToStepTicket.d.ts
+++ dist/requests/ticket/postMoveToStepTicket.d.ts
@@ -1,4 +1,5 @@
 export declare function moveToStepTicket(building: string, ticketId: string, data: {
     workflowDynamicId: number;
+    toStepOrder: number;
     toStepName: string;
 }): Promise<any>;
