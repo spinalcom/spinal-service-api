6689d096955d9a50c99c7b22743a7e77b576a891
spinalcom <a.abdouchakour@spinalcom.com>
2026-03-17 11:01:21 +0100

    Changement du type de reponse pour postNodeParent

--- src/requests/node/postNodeParentByRelation.ts
+++ src/requests/node/postNodeParentByRelation.ts
@@ -2,12 +2,12 @@ import { SpinalAPI } from "../../spinalAPI"; // chemin relatif à src/requests/b
 import { INodeItem } from "./_interfaces";
 
 
-export async function postNodeParentByRelation(buildingId: string, dynamicId: number, relationName: string): Promise<INodeItem> {
+export async function postNodeParentByRelation(buildingId: string, dynamicId: number, relationName: string): Promise<INodeItem[]> {
     try {
             const spinalAPI = SpinalAPI.getInstance();
             const body = [relationName];
             const url = spinalAPI.createUrlWithPlatformId(buildingId, `api/v1/node/${dynamicId}/parents`);
-            const response = await spinalAPI.post<INodeItem>(url, body);
+            const response = await spinalAPI.post<INodeItem[]>(url, body);
             return response.data;
     } catch (error) {
         throw error;
