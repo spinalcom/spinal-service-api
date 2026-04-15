964e86a6cc2e7a1753ad57d682dac9d347c96b97
spinalcom <a.abdouchakour@spinalcom.com>
2026-04-15 09:46:33 +0200

    new request: nodeContext children

---
+++ dist/requests/node/postNodeContextChildrenMultiple.d.ts
@@ -0,0 +1,2 @@
+import { ParentMultipleRequest, ParentMultipleResponse } from "./_interfaces";
+export declare function postNodeContextChildrenMultiple(buildingId: string, contextId: number, body: ParentMultipleRequest[]): Promise<ParentMultipleResponse[]>;
