259c5961b3ed54c67b4d353006f952762dd38831
M <g.mazeau@spinalcom.com>
2026-06-11 13:11:05 +0200

    inventory

--- src/requests/building/postBuildingInventory.ts
+++ src/requests/building/postBuildingInventory.ts
@@ -19,7 +19,7 @@ export async function postBuildingInventory(body: IInventoryBody, options: any):
     if (options.includeArea !== undefined) params.append("includeArea", String(options.includeArea));
     if (options.includeUnassignedItems !== undefined) params.append("includeUnassignedItems", String(options.includeUnassignedItems));
     if (options.onlyDynamicId !== undefined) params.append("onlyDynamicId", String(options.onlyDynamicId));
-
+    if (options.onlyCounts !== undefined) params.append("onlyCounts", String(options.onlyCounts));
     try {
         const response = await spinalAPI.post<any>(`${url}?${params.toString()}`, body);
         return response.data;
